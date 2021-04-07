4b24b6940ab9481e4c7a2bb15d5d47b77564b7ff
shuji watanabe <rinkioutouhenmonzizai@yahoo.co.jp>
2021-04-07 18:08:01 +0900

    fix provider position

--- src/App.js
+++ src/App.js
@@ -10,23 +10,48 @@ import PrivacyPolicy from './components/common/privacypolicy'
 import About from './components/common/about'
 import Contact from './components/common/contact'
 import TemplatePage from './components/main/templatePage'
-
+import { useAuth } from './provider/authProvider'
+import { useError } from './provider/errorProvider'
+import axiosbase from './utils/axiosbase'
 const App = () => {
+  const auth = useAuth();
+  const error = useError();
+  const [loading, setLoading] = React.useState(false)
+  React.useEffect(() => {
+    const authenticate = async () => {
+      await axiosbase.post('/users/authenticate')
+        .then((res) => {
+          error.init()
+          if (res.data === 'authenticated') {
+            auth.authenticated()
+          } else {
+            auth.notAuthenticated()
+          }
+          setLoading(true)
+        }).catch(e => {
+          // error.setError(e)
+          // auth.notAuthenticated()
+          setLoading(true)
+        })
+    }
+    authenticate();
+    // eslint-disable-next-line
+  }, [])
   return (
     <Router>
       <div className="App">
-        <Provider>
-          <ErrorAlert />
-          <Switch>
-            <Route path="/signup" component={SignUp} />
-            <Route path="/signin" component={SignIn} />
-            <Route path="/terms" component={Terms} />
-            <Route path="/privacypolicy" component={PrivacyPolicy} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route path='/' component={TemplatePage} />
-          </Switch>
-        </Provider>
+        {/* <Provider> */}
+        <ErrorAlert />
+        <Switch>
+          <Route path="/signup" component={SignUp} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/terms" component={Terms} />
+          <Route path="/privacypolicy" component={PrivacyPolicy} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path='/' component={TemplatePage} />
+        </Switch>
+        {/* </Provider> */}
       </div>
     </Router >
   );
--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from './provider/totalProvider'
 
 ReactDOM.render(
-  <App />,
+  <Provider>
+    <App />
+  </Provider>
+  ,
   document.getElementById('root')
 );
 
