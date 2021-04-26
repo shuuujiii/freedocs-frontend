4b6671b7f0f006a4b37fcec4fb57e7420ca53f0b
shuji watanabe <rinkioutouhenmonzizai@yahoo.co.jp>
2021-04-26 12:32:37 +0900

    show env

--- src/App.js
+++ src/App.js
@@ -63,6 +63,7 @@ const App = () => {
   return (
     loading ? <Loading /> :
       <div className="App">
+        <div>{'envi:' + process.env.REACT_APP_API}</div>
         <Header search={search}
           setSearch={setSearch}
         />
