32635698bced4a4e0a38db8b78af8350e661f064
shuji watanabe <rinkioutouhenmonzizai@yahoo.co.jp>
2021-05-12 16:27:08 +0900

    add create sort

--- src/components/article/sortReducer.js
+++ src/components/article/sortReducer.js
@@ -1,60 +1,68 @@
 import React from 'react'
 export const SORT = {
-    URL_DESC: "URL_DESC",
-    URL_ASC: "URL_ASC",
+    // URL_DESC: "URL_DESC",
+    // URL_ASC: "URL_ASC",
     LIKES_DESC: "LIKES_DESC",
     LIKES_ASC: "LIKES_ASC",
-    GOOD_DESC: "GOOD_DESC",
-    GOOD_ASC: "GOOD_ASC",
-    BAD_DESC: "BAD_DESC",
-    BAD_ASC: "BAD_ASC",
+    CREATED_AT_DESC: 'CREATE_AT_DESC',
+    CREATED_AT_ASC: 'CREATE_AT_ASC',
+    // GOOD_DESC: "GOOD_DESC",
+    // GOOD_ASC: "GOOD_ASC",
+    // BAD_DESC: "BAD_DESC",
+    // BAD_ASC: "BAD_ASC",
 }
 const initialSortState = {
-    key: 'url',
-    // value: SORT.URL_ASC,
-    order: 'asc',
+    key: 'likeCount',
+    value: SORT.LIKES_DESC,
+    order: 'desc',
 }
 const sortReducer = (state, action) => {
+    // console.log('sortreducer')
     switch (action.type) {
-        case SORT.URL_DESC:
-            return {
-                key: 'url',
-                // value: SORT.URL_DESC,
-                order: 'desc',
-            };
-        case SORT.URL_ASC:
-            return initialSortState
+        // case SORT.URL_DESC:
+        //     return {
+        //         key: 'url',
+        //         // value: SORT.URL_DESC,
+        //         order: 'desc',
+        //     };
+        // case SORT.URL_ASC:
+        //     return initialSortState
         case SORT.LIKES_DESC:
             return {
                 key: 'likeCount',
+                value: SORT.LIKES_DESC,
                 order: 'desc',
             }
         case SORT.LIKES_ASC:
             return {
                 key: 'likeCount',
+                value: SORT.LIKES_ASC,
                 order: 'asc',
             }
-        case SORT.GOOD_DESC:
+        case SORT.CREATED_AT_DESC:
             return {
-                key: 'goodCount',
+                key: 'createdAt',
+                value: SORT.CREATED_AT_DESC,
                 order: 'desc',
             }
-        case SORT.GOOD_ASC:
+        case SORT.CREATED_AT_ASC:
             return {
-                key: 'goodCount',
-                order: 'asc',
-            }
-        case SORT.BAD_DESC:
-            return {
-                key: 'badCount',
-                order: 'desc',
-            }
+                key: 'createdAt',
+                value: SORT.CREATED_AT_ASC,
 
-        case SORT.BAD_ASC:
-            return {
-                key: "badCount",
                 order: 'asc',
             }
+        // case SORT.GOOD_DESC:
+        //     return {
+        //         key: 'goodCount',
+        //         order: 'desc',
+        //     }
+        // case SORT.GOOD_ASC:
+        //     return {
+        //         key: 'goodCount',
+        //         order: 'asc',
+        //     }
+
         default:
             return initialSortState;
     }
