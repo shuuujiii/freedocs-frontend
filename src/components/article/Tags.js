7e92b4c67e347f87819336e2506e6a90f96f69df
shuji watanabe <rinkioutouhenmonzizai@yahoo.co.jp>
2021-05-14 18:20:41 +0900

    change to case-sensitive

---
+++ src/components/article/Tags.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core/styles';
+// import Paper from '@material-ui/core/Paper';
+import { TextField } from '@material-ui/core';
+import Chip from '@material-ui/core/Chip';
+
+// utils
+import axiosbase from '../../utils/axiosbase'
+import { useError } from '../../provider/errorProvider';
+import { useHistory } from 'react-router-dom'
+// component
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        justifyContent: 'left',
+        flexWrap: 'wrap',
+        '& > *': {
+            margin: theme.spacing(0.5),
+        },
+        boxSizing: 'border-box'
+    },
+    chip: {
+        marginRight: theme.spacing(0.5),
+    },
+}));
+
+export const Tags = ({ tags, setTags }) => {
+    const classes = useStyles();
+    const error = useError();
+    const [inputTag, setInputTag] = React.useState('')
+    const handleKeyPress = (e) => {
+        error.init()
+        if (e.key === 'Enter') {
+            e.preventDefault()
+            axiosbase.post('/tag', {
+                name: inputTag
+            })
+                .then(res => {
+                    console.log(res.data)
+                    setTags(prev => [...prev, res.data])
+                    setInputTag('')
+                }).catch(error.setError)
+        }
+    }
+
+    const handleDelete = (id) => {
+        setTags(prev => prev.filter(tag => tag._id !== id))
+    }
+
+    return (
+        <div className={classes.root}>
+            {tags.map(tag => {
+                return (
+                    <Chip
+                        key={tag._id}
+                        className={classes.chip}
+                        label={tag.name}
+                        onDelete={() => { handleDelete(tag._id) }}
+                    />
+                )
+            })}
+            <TextField
+                style={{ outlineWidth: '0' }}
+                value={inputTag}
+                onChange={e => { setInputTag(e.target.value) }}
+                onKeyPress={handleKeyPress}
+                placeholder={'input tag'}
+            />
+        </div>
+    )
+}
+
+Tags.propTypes = {
+    tags: PropTypes.array,
+    setTags: PropTypes.func,
+}
+
+export const TagChips = ({ tags }) => {
+    const classes = useStyles();
+    const history = useHistory()
+
+    const onClickChip = (tagname) => {
+        history.push(`/lists?tag=${tagname}`)
+    }
+    return (
+        <div className={classes.root}>
+            {tags.map(tag => {
+                return (
+                    <Chip
+                        key={tag._id}
+                        className={classes.chip}
+                        label={tag.name}
+                        onClick={() => { onClickChip(tag.name) }}
+                    />
+                )
+            })}
+        </div>
+    );
+}
+TagChips.propTypes = {
+    tags: PropTypes.array,
+}
+
