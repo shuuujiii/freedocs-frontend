06d222d518896fefb56d2fabf32364c98b56acf8
shuji watanabe <rinkioutouhenmonzizai@yahoo.co.jp>
2021-05-14 18:42:36 +0900

    console ranking

--- src/components/page/Home.js
+++ src/components/page/Home.js
@@ -68,7 +68,7 @@ export default function Home() {
         const getRankingData = async () => {
             const res = await axiosbase.get('/article/ranking')
             if (mounted) {
-                // console.log('ranking data', res.data)
+                console.log('ranking data', res.data)
                 setLikesArticles(res.data.likesRanking)
                 setVoteArticles(res.data.voteRanking)
             }
